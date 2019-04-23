bcc1ed513652eb30bb1c4a81b78bd125de44bfa7
Sohail Hussain <sohailsaihail125@gmail.com>
2019-04-23 02:49:39 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Family-GPS-Tracker
+
+1) Type npm install or yarn install command in cmd
+
+2) Setup your firebase API key in config folder
+
+3) Enter in your App Id in logIn function in SignInScreen 
+
---
+++ config/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+    FirebaseConfig: {
+        apiKey: "AIzaSyDGo9UsLL97-318aP_B73VfFcQ1N86uu-o",
+        authDomain: "productapp-e83fa.firebaseapp.com",
+        databaseURL: "https://productapp-e83fa.firebaseio.com",
+        projectId: "productapp-e83fa",
+        storageBucket: "productapp-e83fa.appspot.com",
+        messagingSenderId: "121783964377"
+    }
+}
